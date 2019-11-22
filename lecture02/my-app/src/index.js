034fc0208fbc4297da047cce1f7349ba76d1fa92
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-11-23 01:58:34 +0900

    lecture01, lecture02

---
+++ lecture02/my-app/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+const formatName = function(name){
+    return name.firstName + " " + name.lastName;
+}
+
+const name = {
+    firstName : "Minji",
+    lastName : "Jeong",
+};
+
+const element = <h1>Hello, world!, {formatName(name)}</h1>
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
