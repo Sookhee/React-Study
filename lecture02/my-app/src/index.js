3a42b49d8e26a4ce147d92866d5a614a6832de46
Minji Jeong <s2018s38@e-mirim.hs.kr>
2019-11-23 18:45:48 +0900

    lecture02 (실습) 시계 만들기

--- lecture02/my-app/src/index.js
+++ lecture02/my-app/src/index.js
@@ -22,6 +22,16 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+function tick(){
+    const timeElement = (
+        <div>
+            <h1>It is {new Date().toLocaleTimeString()}</h1>
+        </div>
+    );
+    ReactDOM.render(timeElement, document.getElementById('timer'));
+}
+
+setInterval(tick, 1000);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
